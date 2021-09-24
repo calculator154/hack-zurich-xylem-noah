5c5285e2db5269eb2c484615587a42cb4c920271
Chitsanu K <ckhurewa@akselos.com>
2021-09-25 01:16:48 +0200

    With first map.

--- hack-zurich-xylem-noah/src/HomePage.tsx
+++ hack-zurich-xylem-noah/src/HomePage.tsx
@@ -3,15 +3,20 @@ import * as SR from "semantic-ui-react";
 import * as RD from "react-router-dom"
 
 export const HomePage: React.FC<{}> = () => {
+
+
   return (
     <SR.Container fluid>
       <SR.Segment inverted color="yellow" style={{ height: "320px" }}>
         Flood Status: continuous rainfall for 6 hours.
       </SR.Segment>
 
+      <SR.Button as={RD.Link} content="Map" to={"/map"} />
+
       <SR.Segment inverted color="red" style={{ height: "320px" }}>
         User Status: Need Shelter
       </SR.Segment>
+
       <SR.Grid fluid>
         <SR.Grid.Row fluid>
           <SR.Grid.Column width={8}>
--- hack-zurich-xylem-noah/src/MapPage.tsx
+++ hack-zurich-xylem-noah/src/MapPage.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import * as SR from "semantic-ui-react";
+import MapView from "@arcgis/core/views/MapView";
+import EsriMap from "@arcgis/core/Map";
+// import Map from "@arcgis/core/Map";
+
 
 export const MapPage: React.FC<{}> = () => {
-  return (
-    <div>TODO: map page</div>
-  )
+  const mapEl = React.useRef(null);
+
+  React.useEffect(() =>  {
+
+    const map = new EsriMap({
+      basemap: "streets-vector",
+    });
+
+    let view = new MapView({
+      map: map,
+      center: [-118.244, 34.052],
+      zoom: 12,
+      container: mapEl.current
+    })
+
+    return () => {
+      // clean up the map view
+      if (!!view) {
+        view.destroy();
+      }
+    };
+  })
+
+  return <div style={{ height: 400 }} ref={mapEl} />;
 }
